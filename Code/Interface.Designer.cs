b78aedfef01d410f44190491e1962629427126e0
Owen Adrian <akrivus@gmail.com>
2018-08-19 15:33:04 -0500

    Initial commit

---
+++ Code/Interface.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace PidgeonTalk.Code
+{
+    partial class Interface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Interface));
+            this.Output = new System.Windows.Forms.TextBox();
+            this.EntryYes = new System.Windows.Forms.Button();
+            this.EntryNo = new System.Windows.Forms.Button();
+            this.Play = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Output
+            // 
+            this.Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Output.Location = new System.Drawing.Point(12, 12);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(780, 80);
+            this.Output.TabIndex = 0;
+            this.Output.Text = "😃🙌🤝🌎";
+            // 
+            // EntryYes
+            // 
+            this.EntryYes.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EntryYes.ForeColor = System.Drawing.Color.MediumBlue;
+            this.EntryYes.Location = new System.Drawing.Point(12, 98);
+            this.EntryYes.Name = "EntryYes";
+            this.EntryYes.Size = new System.Drawing.Size(387, 63);
+            this.EntryYes.TabIndex = 1;
+            this.EntryYes.Text = "👌";
+            this.EntryYes.UseVisualStyleBackColor = true;
+            // 
+            // EntryNo
+            // 
+            this.EntryNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EntryNo.ForeColor = System.Drawing.Color.DeepPink;
+            this.EntryNo.Location = new System.Drawing.Point(405, 98);
+            this.EntryNo.Name = "EntryNo";
+            this.EntryNo.Size = new System.Drawing.Size(387, 63);
+            this.EntryNo.TabIndex = 2;
+            this.EntryNo.Text = "🙅";
+            this.EntryNo.UseVisualStyleBackColor = true;
+            // 
+            // Play
+            // 
+            this.Play.BackColor = System.Drawing.SystemColors.Control;
+            this.Play.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.Play.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Play.Location = new System.Drawing.Point(12, 374);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(780, 63);
+            this.Play.TabIndex = 3;
+            this.Play.Text = "🗣️";
+            this.Play.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.button1.Location = new System.Drawing.Point(12, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(256, 63);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "🖐";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Goldenrod;
+            this.button2.Location = new System.Drawing.Point(274, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(256, 63);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "🖖";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(536, 167);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(256, 63);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "😭";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(536, 236);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(256, 63);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "👏";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(274, 236);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(256, 63);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "👐";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(12, 236);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(256, 63);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "😃";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(536, 305);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(256, 63);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "⌚";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(274, 305);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(256, 63);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "🙄";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(12, 305);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(256, 63);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "🤔";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // Interface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 448);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.EntryNo);
+            this.Controls.Add(this.EntryYes);
+            this.Controls.Add(this.Output);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Interface";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PidgeonTalk™";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.Button EntryYes;
+        private System.Windows.Forms.Button EntryNo;
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
--- Code/Interface.cs
+++ Code/Interface.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace PidgeonTalk
+namespace PidgeonTalk.Code
 {
-    public partial class Form1 : Form
+    public partial class Interface : Form
     {
-        public Form1()
+        public Interface()
         {
             InitializeComponent();
         }
